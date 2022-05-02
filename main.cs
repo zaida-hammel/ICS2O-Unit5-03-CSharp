67a0294c510d7dc8b6001c6c8fc3ca6624365a83
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-05-02 00:00:31 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Program 
+{
+    public static void Main (string[] args) 
+    {
+        int userAge;
+
+        Console.WriteLine("Write your age:");
+        userAge = int.Parse(Console.ReadLine());
+        if (userAge > 17)
+        {
+            Console.WriteLine("You are eligible to watch R rated movies alone.");
+            Console.ReadLine();
+        }
+        else if (userAge > 12)
+        {
+            Console.WriteLine("You are eligible to watch PG 13 rated movies alone.");
+            Console.ReadLine();
+        }
+        else if (userAge > 4)
+        {
+            Console.WriteLine("You are eligible to watch G and PG rated movies alone.");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("You are eligible to watch G and PG rated movies alone.");
+            Console.ReadLine();
+        }
+    }
+}
