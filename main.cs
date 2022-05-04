5863c9a6434f8860484464869f5daa5986cba290
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-05-04 20:46:45 +0000

    edited code

--- main.cs
+++ main.cs
@@ -11,22 +11,19 @@ class Program
         if (userAge > 17)
         {
             Console.WriteLine("You are eligible to watch R rated movies alone.");
-            Console.ReadLine();
         }
         else if (userAge > 12)
         {
             Console.WriteLine("You are eligible to watch PG 13 rated movies alone.");
-            Console.ReadLine();
         }
         else if (userAge > 4)
         {
             Console.WriteLine("You are eligible to watch G and PG rated movies alone.");
-            Console.ReadLine();
         }
         else
         {
             Console.WriteLine("You are eligible to watch G and PG rated movies alone.");
-            Console.ReadLine();
         }
+      Console.WriteLine("Done.");
     }
 }
